10122688f130cf164d09c636141f54da789f63aa
ArrowOfDevil <47675041+ArrowOfDevil@users.noreply.github.com>
2022-11-25 19:04:40 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+  SetConsoleOutputCP(CP_UTF8);
+  FILE *f1, *f2, *f3, *f4;
+  char fst[] = R"(C:\Users\Dilya\CLionProjects\untitled8\first.txt)";
+  char snd[] = R"(C:\Users\Dilya\CLionProjects\untitled8\second.txt)";
+  char trd[] = R"(C:\Users\Dilya\CLionProjects\untitled8\third.txt)";
+  char ex2[] = R"(C:\Users\Dilya\CLionProjects\untitled8\ex2.txt)";
+  const int N = 512;// максимальный размер строки в файле
+  char ArrChar1[N];
+  char ArrChar2[N];
+
+  f1 = fopen(fst, "r+t");
+  f2 = fopen(snd, "r+t");
+  f3 = fopen(trd, "w+t");
+  f4 = fopen(ex2, "a"); //так как неизвестно существует файл или нет, то сначала создадим его с помощью "а"
+  fclose(f4);
+
+  if (!f1) return printf("error opening file1"), -1; // это законно, но правильно ли с точки зрения код стайла (?)
+  if (!f2) return printf("error opening file2"), -1;
+  if (!f3) return printf("error opening file3"), -1;
+  if (!f4) return printf("error opening file4"), -1;
+
+  fgets(ArrChar1, N, f1);
+  fgets(ArrChar2, N, f2);
+
+  int i = 0, j = 0;
+
+  while ((i < N) && (j < N)) {
+    if (ArrChar1[i] != 0) fputc(ArrChar1[i], f3), ++i;
+    if (ArrChar2[j] != 0) fputc(ArrChar2[j], f3), ++j;
+    if (ArrChar1[i] == 0 && ArrChar2[j] == 0) break;
+  }
+
+  printf("работа с файлом\n");
+  int c;
+  char str[N];
+  printf("1.Записать текст в файл. \n"
+         "2.Дописать текст к концу файла. \n"
+         "3.Просмотреть файл. \n"
+         "4.Удалить файл. \n"
+         "0.Закончить работу. \n");
+  scanf("%d", &c);
+  getchar();
+  while (c != 0) {
+    switch (c) {
+      case 0: break;
+      case 1:f4 = fopen(ex2, "w+t");
+
+        scanf("%s", str);
+        fputs(str, f4);
+        fputs("\n", f4);
+
+        fclose(f4);
+        break;
+      case 2:f4 = fopen(ex2, "a+t");
+
+        scanf("%s", str);
+        fputs(str, f4);
+        fputs("\n", f4);
+
+        fclose(f4);
+        break;
+      case 3:f4 = fopen(ex2, "r+");
+        while (
+            fgets(str, sizeof(str), f4) != nullptr)
+          fputs(str, stdout);
+
+        break;
+      case 4:remove(ex2);
+        break;
+      default:printf("неверная команда");
+    }
+    printf("1.Записать текст в файл.\n"
+           "2.Дописать текст к концу файла.\n"
+           "3.Просмотреть файл.\n"
+           "4.Удалить файл.\n"
+           "0.Закончить работу.\n");
+    scanf("%d", &c);
+  }
+  printf("Работа завершена, спасибо)");
+
+  fclose(f1);
+  fclose(f2);
+  fclose(f3);
+
+  return 0;
+}
